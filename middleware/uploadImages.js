bf79befd5b57a87a2da6a2554ea7a4b364808dd9
mnuralim <shadownur345.com>
2023-03-18 22:57:31 +0700

    update

--- middleware/uploadImages.js
+++ middleware/uploadImages.js
@@ -7,11 +7,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const diskStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./public/images");
+    cb(null, "./cek");
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.fieldname + "-" + uniqueSuffix + ".tmp");
   },
 });
 
