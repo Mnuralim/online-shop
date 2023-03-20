9d763bef028b731f5bfed72fccbc0da057a5cd8b
mnuralim <shadownur345.com>
2023-03-20 18:37:19 +0700

    update

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ db();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(
+  express.json({
+    limit: "10mb",
+  })
+);
 app.use(cookieParser());
 app.use(morgan("dev"));
 
